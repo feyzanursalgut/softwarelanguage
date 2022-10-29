ae7521efeb585e368b1b8116571fd245db5f817c
feyzanursalgut <fsalgut@gmail.com>
2022-10-29 17:37:37 +0300

    Bir takım güncellemeler yapıldı.

--- src/main/java/codespring/swLanguage/webApi/controllers/programmingLanguageController.java
+++ src/main/java/codespring/swLanguage/webApi/controllers/programmingLanguageController.java
@@ -19,7 +19,9 @@ import io.swagger.v3.oas.annotations.*;
 
 @RestController
 @RequestMapping("/api/PLanguages")
+
 public class programmingLanguageController {
+    
     private programmingLanguageService pLanguageService;
 
    @Autowired
@@ -28,31 +30,31 @@ public class programmingLanguageController {
     }
 
     @GetMapping("/getAll")
-    @Operation(summary = "**Kayıtlı datayı getirir**")
+    @Operation(summary = "**Kayıtlı datayı getirir.**" , tags = "Software Languages Application")
     public List<programmingLanguage> getAll() {
         return pLanguageService.getAll();
     }
 
     @GetMapping("/getId")
-    @Operation(summary = "**Kayıtlı datayı Id'sine göre arattığınızda getirir**")
+    @Operation(summary = "**Kayıtlı datayı Id'sine göre arattığınızda getirir.**",tags = "Software Languages Application")
     public programmingLanguage getId(@RequestParam int id) throws Exception{
         return this.pLanguageService.getId(id);
     }
 
     @PostMapping("/setName")
-    @Operation(summary = "**Yeni bir kayıt ekler.Değerler boş gönderilemez**")
+    @Operation(summary = "**Yeni bir kayıt ekler.Değerler boş gönderilemez.**", tags = "Software Languages Application")
     public void pLanguageAdd(@RequestBody programmingLanguage pLanguage)throws Exception{
         this.pLanguageService.pLanguageAdd(pLanguage);
     }
      
     @PutMapping("/updateName")
-    @Operation(summary = "**Kayıtlı datayı günceller. Aynı değer birden fazla eklenemez**")
+    @Operation(summary = "**Kayıtlı datayı günceller. Aynı değer birden fazla eklenemez.**", tags = "Software Languages Application")
     public void pLanguageUpdate(@RequestBody programmingLanguage pLanguage) {
         this.pLanguageService.pLanguageUpdate(pLanguage);
     }
 
     @DeleteMapping("/delete")
-    @Operation(summary = "**Kayıtlı datayı siler**")
+    @Operation(summary = "**Kayıtlı datayı siler.**", tags = "Software Languages Application")
         public void pLanguageDelete(@RequestParam int id){
             this.pLanguageService.pLanguageDelete(id);
     }
